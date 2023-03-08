9dc3315871d249f3ae111c94c8fb4955d0718995
AKI-MIDO <endoakirara@gmail.com>
2023-03-08 15:34:29 +0900

    元に戻した

--- hello.py
+++ hello.py
@@ -3,8 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/japan/<city>") #可変URLルーティング
+"""@app.route("/japan/<city>") #可変URLルーティング
 def japan(city):
-    return f"<p>Hello,{city} japan!!</p>"
+    return f"<p>Hello,{city} japan!!</p>" """
 
-
+@app.route("/") 
+def hello():
+    return "Hello World!!"
